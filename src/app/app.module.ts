5164b939d01b325aa6b1cb99d58c27a8a2cb2c8e
Tejesh <nagatejesh@users.noreply.github.com>
2024-08-17 09:50:29 +0000

    asdfg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserLogoComponent } from './header/user-logo/user-logo.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { HomeComponent } from './home/home.component';
+import { BannerComponent } from './home/banner/banner.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserLogoComponent,
+    UserDetailsComponent,
+    HomeComponent,
+    BannerComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+searchProducts() {
+  throw new Error('Method not implemented.');
+}
+
+}
